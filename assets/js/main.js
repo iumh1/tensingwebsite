f982e9e60dcd875c8da82dcbbd47ae770528347b
iumh1 <andris.eichler@googlemail.com>
2017-05-21 21:13:27 +0200

    Add files via upload
    added a progress bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,11 @@ Script: https://teamtreehouse.com/community/loading-external-html-into-a-div-usi
 var includeDiv = $("#include");
 $(window).on('hashchange', function() {
     var href = location.hash.slice(1) +".html";
+    $("#progressbar").show("fast");
     includeDiv.load('files/' + href);
+    $("#progressbar").hide();
 });
 /*part ended*/
 
 $("#include").load("files/home.html");
+$("#progressbar").hide("100");
