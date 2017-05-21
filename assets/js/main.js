df3d265c1bf2c31ce675129e6ef8e8aa1975dd30
iumh1 <andris.eichler@googlemail.com>
2017-05-21 19:33:15 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
+/*
+Script: https://teamtreehouse.com/community/loading-external-html-into-a-div-using-jquery
+*/
 var includeDiv = $("#include");
 $(window).on('hashchange', function() {
     var href = location.hash.slice(1) +".html";
     includeDiv.load('files/' + href);
 });
+/*part ended*/
 
 $("#include").load("files/home.html");
